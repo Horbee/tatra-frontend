04348a9a8866c983064a6490bb59e808420c5f27
Norbert Horgas <norbert.horgas@redheads.de>
2019-08-19 10:48:55 +0200

    adding days mapper

--- src/pages/home/static-calendar/CalendarComponent.tsx
+++ src/pages/home/static-calendar/CalendarComponent.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import { Grid, makeStyles } from "@material-ui/core";
 
-import { ColorPicker } from "./ColorPicker";
 import { StaticCalendar } from "./StaticCalendar";
 
 const useStyles = makeStyles(theme => ({
@@ -11,43 +10,31 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const dateTemplate = (date: any) => {
-  if (date.day > 10 && date.day < 15) {
-    return (
-      <div
-        style={{
-          backgroundColor: ColorPicker.getColor([3, 0, 10]), //"#1dcbb3",
-          color: "#ffffff",
-          fontWeight: "bold",
-          borderRadius: "50%",
-          width: "2em",
-          height: "2em",
-          lineHeight: "2em",
-          padding: 0
-        }}
-      >
-        {date.day}
-      </div>
-    );
-  } else {
-    return date.day;
-  }
-};
-
 export const CalendarComponent = () => {
   const classes = useStyles();
-
-  console.log(ColorPicker.getColor([2, 1, 3]));
 
   return (
     <Grid container justify="center">
       <Grid item className={classes.calendarItem}>
-        <StaticCalendar showDate={new Date("09-01-2019")} />
+        <StaticCalendar
+          showDate={new Date("09-01-2019")}
+          daysMap={[
+            { arrayIndex: 0, daysToMap: [7, 8] },
+            { arrayIndex: 1, daysToMap: [14, 15] },
+            { arrayIndex: 2, daysToMap: [21, 22] },
+            { arrayIndex: 3, daysToMap: [28, 29] }
+          ]}
+        />
       </Grid>
       <Grid item>
         <StaticCalendar
           showDate={new Date("10-02-2019")}
-          dateTemplate={dateTemplate}
+          daysMap={[
+            { arrayIndex: 4, daysToMap: [5, 6] },
+            { arrayIndex: 5, daysToMap: [12, 13] },
+            { arrayIndex: 6, daysToMap: [19, 20] },
+            { arrayIndex: 7, daysToMap: [26, 27] }
+          ]}
         />
       </Grid>
     </Grid>
--- src/pages/home/static-calendar/StaticCalendar.tsx
+++ src/pages/home/static-calendar/StaticCalendar.tsx
@@ -2,23 +2,38 @@ import { Calendar } from "primereact/calendar";
 import React, { useState } from "react";
 
 import { Locale } from "./CalendarUtils";
+import { ColorPicker } from "./ColorPicker";
 
 interface StaticCalendarProps {
   showDate: Date;
-  dateTemplate?: any;
+  daysMap: { arrayIndex: number; daysToMap: [number, number] }[];
 }
 
-export const StaticCalendar = ({
-  showDate,
-  dateTemplate
-}: StaticCalendarProps) => {
+export const StaticCalendar = ({ showDate }: StaticCalendarProps) => {
   const [date, changeDate] = useState(showDate);
 
-  const dates = [
-    new Date("10-01-2019"),
-    new Date("10-02-2019"),
-    new Date("10-03-2019")
-  ];
+  const dateTemplate = (date: any) => {
+    if (date.day > 10 && date.day < 15) {
+      return (
+        <div
+          style={{
+            backgroundColor: ColorPicker.getColor([3, 0, 10]), //"#1dcbb3",
+            color: "#ffffff",
+            fontWeight: "bold",
+            borderRadius: "50%",
+            width: "2em",
+            height: "2em",
+            lineHeight: "2em",
+            padding: 0
+          }}
+        >
+          {date.day}
+        </div>
+      );
+    } else {
+      return date.day;
+    }
+  };
 
   const handleDateChanged = (e: {
     originalEvent: Event;
@@ -33,7 +48,6 @@ export const StaticCalendar = ({
       inline={true}
       readOnlyInput={true}
       selectionMode="multiple"
-      value={dates}
       onChange={handleDateChanged}
       viewDate={date}
       dateTemplate={dateTemplate}
