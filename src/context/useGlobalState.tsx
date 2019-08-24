3ac8d45ee901e3ff90a4d9cdb9839e1c535f7316
Norbert H <n.horox@gmail.com>
2019-08-24 21:32:31 +0200

    feat: adding global constants

--- src/context/useGlobalState.tsx
+++ src/context/useGlobalState.tsx
@@ -1,11 +1,10 @@
 import { useReducer } from "react";
 
-import { Availability } from "../models/Availability";
 import {
-  CHANGE_AVAILABILITY,
-  FETCH_AVAILABILITIES,
-  GlobalActionTypes
-} from "./actions";
+    NUM_WEEKS, VOTES_INDEX_GOOD, VOTES_INDEX_MAYBE, VOTES_INDEX_NOT_GOOD
+} from "../constants/globals";
+import { Availability } from "../models/Availability";
+import { CHANGE_AVAILABILITY, FETCH_AVAILABILITIES, GlobalActionTypes } from "./actions";
 
 export interface IGlobalState {
   availabilityArray: Availability[];
@@ -17,18 +16,18 @@ const getVotes = (
 ): [number, number, number][] => {
   const result: Array<[number, number, number]> = [];
 
-  for (let index = 0; index < 8; index++) {
+  for (let index = 0; index < NUM_WEEKS; index++) {
     result.push([0, 0, 0]);
   }
 
   for (let availability of availabilityArray) {
     availability.availabilities.forEach(element => {
-      let innerIndex = 1; // Maybe
+      let innerIndex = VOTES_INDEX_MAYBE; // Maybe
 
       if (element.status === 1) {
-        innerIndex = 2; // Good
+        innerIndex = VOTES_INDEX_GOOD; // Good
       } else if (element.status === 2) {
-        innerIndex = 0; // Not Good
+        innerIndex = VOTES_INDEX_NOT_GOOD; // Not Good
       }
 
       result[element.week][innerIndex]++;
