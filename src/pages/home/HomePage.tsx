efb9a6b6c16b6d6d65b6e502cf78bdfcd27db549
Norbert H <n.horox@gmail.com>
2019-08-25 14:34:17 +0200

    chore: optimize legend component

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 import { Container, CssBaseline, makeStyles, Paper } from "@material-ui/core";
 
-import { CustomChip } from "./chip/CustomChip";
 import { CustomTable } from "./custom-table/CustomTable";
 import { Footer } from "./Footer";
 import { HeroUnit } from "./HeroUnit";
+import { Legend } from "./legend/Legend";
 import { CalendarComponent } from "./static-calendar/CalendarComponent";
 
 const useStyles = makeStyles(theme => ({
@@ -29,7 +29,7 @@ export const HomePage = () => {
         <Container className={classes.tableGrid} maxWidth="lg">
           <Paper className={classes.calendarPaper}>
             <CalendarComponent />
-            <CustomChip />
+            <Legend />
           </Paper>
           <Paper style={{ width: "100%", overflowX: "auto" }}>
             <CustomTable />
