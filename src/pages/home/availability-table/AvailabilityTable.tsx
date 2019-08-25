1f33e856d62130b65587ed8a742d484f63e2e6bc
Norbert H <n.horox@gmail.com>
2019-08-25 14:55:54 +0200

    chore: remove unnecesarry div

--- src/pages/home/availability-table/AvailabilityTable.tsx
+++ src/pages/home/availability-table/AvailabilityTable.tsx
@@ -33,57 +33,55 @@ export const AvailabilityTable = () => {
     state: { availabilityArray: data }
   } = useContext(GlobalStateContext);
   return (
-    <div>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Nevek / Dátumok</TableCell>
-            <TableCell align="right">Szept. 7-8 </TableCell>
-            <TableCell align="right">Szept. 14-15</TableCell>
-            <TableCell align="right">Szept. 21-22</TableCell>
-            <TableCell align="right">Szept. 28-29</TableCell>
-            <TableCell align="right">Okt. 5-6</TableCell>
-            <TableCell align="right">Okt. 12-13</TableCell>
-            <TableCell align="right">Okt. 19-20</TableCell>
-            <TableCell align="right">Okt. 26-27</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map(row => {
-            return (
-              <TableRow key={row.personName}>
-                <TableCell component="th" scope="row">
-                  {row.personName}
-                </TableCell>
-                {weeks.map(week => {
-                  const result = row.availabilities.find(
-                    availability => availability.week === week
-                  );
-                  return result ? (
-                    <TableCell align="right" key={row.personName + week}>
-                      <PickerTableCell
-                        id={row.id}
-                        personName={row.personName}
-                        week={week}
-                        availability={result.status}
-                      />
-                    </TableCell>
-                  ) : (
-                    <TableCell align="right" key={row.personName + week}>
-                      <PickerTableCell
-                        id={row.id}
-                        personName={row.personName}
-                        week={week}
-                        availability={0}
-                      />
-                    </TableCell>
-                  );
-                })}
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </div>
+    <Table className={classes.table}>
+      <TableHead>
+        <TableRow>
+          <TableCell>Nevek / Dátumok</TableCell>
+          <TableCell align="right">Szept. 7-8 </TableCell>
+          <TableCell align="right">Szept. 14-15</TableCell>
+          <TableCell align="right">Szept. 21-22</TableCell>
+          <TableCell align="right">Szept. 28-29</TableCell>
+          <TableCell align="right">Okt. 5-6</TableCell>
+          <TableCell align="right">Okt. 12-13</TableCell>
+          <TableCell align="right">Okt. 19-20</TableCell>
+          <TableCell align="right">Okt. 26-27</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {data.map(row => {
+          return (
+            <TableRow key={row.personName}>
+              <TableCell component="th" scope="row">
+                {row.personName}
+              </TableCell>
+              {weeks.map(week => {
+                const result = row.availabilities.find(
+                  availability => availability.week === week
+                );
+                return result ? (
+                  <TableCell align="right" key={row.personName + week}>
+                    <PickerTableCell
+                      id={row.id}
+                      personName={row.personName}
+                      week={week}
+                      availability={result.status}
+                    />
+                  </TableCell>
+                ) : (
+                  <TableCell align="right" key={row.personName + week}>
+                    <PickerTableCell
+                      id={row.id}
+                      personName={row.personName}
+                      week={week}
+                      availability={0}
+                    />
+                  </TableCell>
+                );
+              })}
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </Table>
   );
 };
