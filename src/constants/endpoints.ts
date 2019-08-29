8a6ad1e457189ea843aeb6177e984b12f2aae0d9
Norbert H <n.horox@gmail.com>
2019-08-29 10:14:07 +0200

    chore: change endpoint to prod befora merge

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,6 +1,6 @@
 export abstract class Endpoints {
-  //public static APP_BASE_URL = "https://tatra-tura-2019.firebaseio.com/";
-  public static APP_BASE_URL = "https://tatra-tura-2019-dev.firebaseio.com/";
+  public static APP_BASE_URL = "https://tatra-tura-2019.firebaseio.com/";
+  //public static APP_BASE_URL = "https://tatra-tura-2019-dev.firebaseio.com/";
   public static AVAILABILITIES = `${Endpoints.APP_BASE_URL}availabilities.json`;
   public static AVAILABILITY = `${Endpoints.APP_BASE_URL}availabilities/`;
 }
